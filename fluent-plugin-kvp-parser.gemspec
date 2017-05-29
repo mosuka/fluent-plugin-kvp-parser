14f2054168739f3a986d6ba4c726dc348e208e19
Minoru Osuka <minoru.osuka@gmail.com>
2017-05-29 12:29:14 +0900

    first commit

---
+++ fluent-plugin-kvp-parser.gemspec
@@ -0,0 +1,23 @@
+# coding: utf-8
+lib = File.expand_path('../lib', __FILE__)
+$LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
+
+Gem::Specification.new do |spec|
+  spec.name          = "fluent-plugin-kvp-parser"
+  spec.version       = "0.0.1"
+  spec.description   = 'Fluentd parser plugin to parse key value pairs'
+  spec.authors       = ["mosuka"]
+  spec.email         = ["minoru.osuka@gmail.com"]
+  spec.summary       = %q{Fluentd parser plugin to parse key value pairs}
+  spec.homepage      = "https://github.com/mosuka/fluent-plugin-kvp-parser"
+  spec.license       = "Apache License, Version 2.0"
+
+  spec.files         = `git ls-files`.split($/)
+  spec.executables   = spec.files.grep(%r{^bin/}) { |f| File.basename(f) }
+  spec.test_files    = spec.files.grep(%r{^(test|spec|features)/})
+  spec.require_paths = ["lib"]
+
+  spec.add_development_dependency "bundler", '~> 1.15.0'
+  spec.add_development_dependency "rake", '~> 11.1.2'
+  spec.add_runtime_dependency "fluentd", ['>= 0.10.0', '< 0.14.0']
+end
---
+++ lib/fluent/plugin/parser_kvp.rb
@@ -0,0 +1,58 @@
+module Fluent
+  class TextParser
+    class KVPParser < Parser
+      include Configurable
+      include TypeConverter
+
+      Plugin.register_parser('kvp', self)
+
+      config_param :kv_delimiter, :string, :default => '\t\s'
+      config_param :kv_char, :string, :default => '='
+      config_param :time_key, :string, :default => 'time'
+
+      def configure(conf={})
+        super
+        if @kv_delimiter[0] == '/' and @kv_delimiter[-1] == '/'
+          @kv_delimiter = Regexp.new(@kv_delimiter[1..-2])
+        end
+
+        @kv_regex_str = '("(?:(?:\\\.|[^"])*)"|(?:[^' + @kv_delimiter + ']*))\s*' + @kv_char + '\s*("(?:(?:\\\.|[^"])*)"|(?:[^' + @kv_delimiter + ']*))'
+        @kv_regex = Regexp.new(@kv_regex_str)
+      end
+
+      def parse(text)
+        record = {}
+
+        text.scan(@kv_regex) do | m |
+          k = (m[0][0] == '"' and m[0][-1] == '"') ? m[0][1..-2] : m[0]
+          v = (m[1][0] == '"' and m[1][-1] == '"') ? m[1][1..-2] : m[1]
+          record[k] = v
+        end
+
+        convert_field_type!(record) if @type_converters
+        time = record.delete(@time_key)
+        if time.nil?
+          time = Engine.now
+        elsif time.respond_to?(:to_i)
+          time = time.to_i
+        else
+          raise RuntimeError, "The #{@time_key}=#{time} is a bad time field"
+        end
+
+        yield time, record
+      end
+
+      private
+
+      def convert_field_type!(record)
+        @type_converters.each_key { |key|
+          if value = record[key]
+            record[key] = convert_type(key, value)
+          end
+        }
+      end
+
+    end
+    register_template('kvp', Proc.new { KVPParser.new })
+  end
+end
